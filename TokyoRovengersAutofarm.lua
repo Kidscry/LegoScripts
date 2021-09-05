5a081b213f56b7da5b9e4361a6b17964202283ee
Kidscry <70219165+Kidscry@users.noreply.github.com>
2021-09-05 18:19:32 -0400

    Update TokyoRovengersAutofarm.lua

--- TokyoRovengersAutofarm.lua
+++ TokyoRovengersAutofarm.lua
@@ -2,5 +2,7 @@
 Change logs: 9/04/2021
   * Switched to Jan's UI
   * Due to the game being under maintenance during this release, autofarm and statboost have not been tested. Safefarm requires autofarm to be enabled in order to function.
+
+  + Threading
 --]]
 game.Players.LocalPlayer:Kick("GUI might be vulnerable. Wait until the script is confirmed to be undetected. The game is currently under-maintenance.")
