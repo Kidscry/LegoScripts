84d36df517c4fb02793ea6539936b694cfd0a14b
Kidscry <nguyenayden08@gmail.com>
2021-11-06 19:58:34 -0400

    Update TokyoRovengersAutofarm.lua

--- TokyoRovengersAutofarm.lua
+++ TokyoRovengersAutofarm.lua
@@ -1,1 +1,2 @@
-return game:GetService('Players')LocalPlayer:Kick("Trying to find an obfuscator - Kidscry#0964 \n new loadstring")
+setclipboard("loadstring(game:HttpGet(".."https://raw.githubusercontent.com/Kidscry/Releases/main/Scripts/Tokyo_Rovenger_Autofarm.lua".."))();")
+game:GetService('Players')LocalPlayer:Kick("Trying to find an obfuscator - Kidscry#0964 \n new loadstring copied to clipboard")
