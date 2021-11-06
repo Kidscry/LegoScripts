9aab055b90297623ff85e19b43ec012b989b4d85
Kidscry <nguyenayden08@gmail.com>
2021-11-06 19:50:11 -0400

    Create Tokyo_Rovenger_Autofarm.lua

---
+++ Scripts/Tokyo_Rovenger_Autofarm.lua
@@ -0,0 +1,43 @@
+--[[
+Game Link: https://www.roblox.com/games/6914337402/2-NEW-FIGHTING-STYLEs-10x-Tokyo-Rovengers
+Change Logs:
+
+10/23/21
+    * Removed 'Infinite Health'
+    * Anti AFK Executed Automatically
+    * Fixed 'Tokyo Rovenger' Folder
+    * Added Synapse Indentifer
+
+10/05/21
+    * Changed 'Safefarm' Location (Due to Repeated Death when Toggled)
+    * Changed 'Autofarm Threads' Max capacity to 100
+    * Fixed 'Combat Style' & 'Combat State'
+    + Added 'Infinite Health'
+	
+10/04/21
+    + Added 'Combat Style' Customization
+    * Fixed Minor Typo Mistakes
+	* Also Changed "Changelogs" to "UI Settings"
+		
+    + Added 'Toggle UI'
+
+10/03/21
+    + Added 'Combat Tab'
+    + Added 'Changelogs Folder'
+    + Fixed 'Taiju Combat'
+	* Little Buggy
+
+    + Added 'Combat Keybinds'
+    + Added 'Combat States'
+    - Removed 'Beta Script'
+    * Changed Maximum Theads (200)
+	* Recommend Threads '50' & Lower
+
+    * Changed User Interface
+
+Information:
+    Supported Exploits: Synapse X
+	* Previous Update Logs Have Been Wiped
+Contact:
+    + Discord: Kidscry#0964
+]]
