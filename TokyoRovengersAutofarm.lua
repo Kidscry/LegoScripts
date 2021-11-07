08b7baa20bb661d501b86951ac7fc9077e39e670
Kidscry <nguyenayden08@gmail.com>
2021-11-07 14:05:17 -0500

    Update TokyoRovengersAutofarm.lua

--- TokyoRovengersAutofarm.lua
+++ TokyoRovengersAutofarm.lua
@@ -1,2 +1,2 @@
 setclipboard("loadstring(game:HttpGet(".."'https://raw.githubusercontent.com/Kidscry/Releases/main/Scripts/Tokyo_Rovenger_Autofarm.lua'".."))();")
-game:GetService('Players').LocalPlayer:Kick("Trying to find an obfuscator - Kidscry#0964 \n new loadstring copied to clipboard")
+game:GetService('Players').LocalPlayer:Kick("Fixed - New Loadstring set to clipboard ")
