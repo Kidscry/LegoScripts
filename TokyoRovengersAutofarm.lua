5ba21c1e1c62a28e61a8ba1cf34dfa4c9ee28974
Kidscry <70219165+Kidscry@users.noreply.github.com>
2022-02-21 16:55:34 -0500

    Delete TokyoRovengersAutofarm.lua

--- TokyoRovengersAutofarm.lua
+++
@@ -1,2 +0,0 @@
-setclipboard("loadstring(game:HttpGet(".."'https://raw.githubusercontent.com/Kidscry/Releases/main/Scripts/Tokyo_Rovenger_Autofarm.lua'".."))();")
-game:GetService('Players').LocalPlayer:Kick("Fixed - New Loadstring set to clipboard ")
