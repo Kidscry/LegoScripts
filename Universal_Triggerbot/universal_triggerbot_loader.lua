5cf84949b81fd07876d1eb11d6e3f3301bdfdecd
Kidscry <70219165+Kidscry@users.noreply.github.com>
2023-03-12 22:46:09 -0400

    Update universal_triggerbot_loader.lua

--- Universal_Triggerbot/universal_triggerbot_loader.lua
+++ Universal_Triggerbot/universal_triggerbot_loader.lua
@@ -1,3 +1,8 @@
+--[[
+	code generated using luamin.js, Herrtt#3868
+--]]
+
+
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kidscry/Releases/main/Utilities/UI.lua"))();
 
 local Players = game:GetService("Players")
@@ -5,36 +10,93 @@ local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
-local TriggerbotEnabled = false
+
+-- stolen shit from wally
+if shared._unload then
+	pcall(shared._unload)
+end
+function shared._unload()
+	if shared._id then
+		pcall(RunService.UnbindFromRenderStep, RunService, shared._id)
+	end
+	if library.open then
+		library:Close()
+	end
+	library.base:ClearAllChildren()
+	library.base:Destroy()
+end
 
 local function OnRenderStepped()
-    local target = Mouse.Target
-    if target and target.Parent and target.Parent:IsA("Model") and target.Parent:FindFirstChild("Humanoid") then
-        local humanoid = target.Parent:FindFirstChild("Humanoid")
-        if humanoid and humanoid.Health > 0 and target.Parent ~= LocalPlayer.Character then
-            local raycastParams = RaycastParams.new()
-            raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
-            raycastParams.FilterDescendantsInstances = {LocalPlayer.Character}
-            local raycastResult = workspace:Raycast(LocalPlayer.Character.Head.Position, target.Position - LocalPlayer.Character.Head.Position, raycastParams)
-            if raycastResult and raycastResult.Instance == target and TriggerbotEnabled then
-                mouse1press()
-                wait()
-                mouse1release()
-            end
-        end
-    end
+	local target = Mouse.Target
+	if target and target.Parent and target.Parent:IsA("Model") and target.Parent:FindFirstChild("Humanoid") then
+		local humanoid = target.Parent:FindFirstChild("Humanoid")
+		if humanoid and humanoid.Health > 0 and target.Parent ~= LocalPlayer.Character then
+			local raycastParams = RaycastParams.new()
+			raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+			raycastParams.FilterDescendantsInstances = {
+				LocalPlayer.Character
+			}
+			local raycastResult = workspace:Raycast(LocalPlayer.Character.Head.Position, target.Position - LocalPlayer.Character.Head.Position, raycastParams)
+			if raycastResult and raycastResult.Instance == target and (library.flags.Triggerbot) then
+				mouse1press()
+				wait(library.flags.Delay)
+				mouse1release()
+			end
+		end
+	end
 end
+RunService.RenderStepped:Connect(OnRenderStepped)
 
 local Main = library:CreateWindow("MAIN")
 
-Main:AddToggle({
-    text = "Triggerbot",
-    flag = "Triggerbot",
-    callback = function(enabled)
-        TriggerbotEnabled = enabled
-    end
+local TriggerbotToggle = Main:AddToggle({
+	text = "Triggerbot",
+	flag = "Triggerbot",
 })
 
-RunService.RenderStepped:Connect(OnRenderStepped)
+local Settings = library:CreateWindow("CONFIGURATIONS")
+Settings:AddBind({ 
+	text = 'Triggerbot Keybind',
+	key = Enum.KeyCode.Two,
+	callback = function()
+		TriggerbotToggle:SetState(not TriggerbotToggle.state)
+	end
+})
+Settings:AddSlider({
+	text = 'Triggerbot Delay',
+	flag = 'Delay',
+	min = 0,
+	max = 2,
+})
+
+local Credits = library:CreateWindow("CREDITS")
+
+Credits:AddLabel({
+	text = 'Jan - UI library'
+})
+Credits:AddLabel({
+	text = 'Kidscry - Script'
+})
+Credits:AddLabel({
+	text = 'Version 1.0'
+})
+Credits:AddLabel({
+	text = 'Updated 3/12/23'
+})
+Credits:AddDivider()
+Credits:AddButton({
+	text = 'Unload script',
+	callback = function()
+		shared._unload()
+	end
+})
+Credits:AddDivider()
+Credits:AddBind({
+	text = 'Menu toggle',
+	key = Enum.KeyCode.Delete,
+	callback = function()
+		library:Close()
+	end
+})
 
 library:Init()
