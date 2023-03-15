7fece956cfb164a52b04d0e0e135a8896b37bd58
Kidscry <70219165+Kidscry@users.noreply.github.com>
2023-03-15 16:51:12 -0400

    Update universal_triggerbot_loader.lua

--- Universal_Triggerbot/universal_triggerbot_loader.lua
+++ Universal_Triggerbot/universal_triggerbot_loader.lua
@@ -1,18 +1,26 @@
+--[[
+ __  __     __     _____     ______     ______     ______     __  __    
+/\ \/ /    /\ \   /\  __-.  /\  ___\   /\  ___\   /\  == \   /\ \_\ \   
+\ \  _"-.  \ \ \  \ \ \/\ \ \ \___  \  \ \ \____  \ \  __<   \ \____ \  
+ \ \_\ \_\  \ \_\  \ \____-  \/\_____\  \ \_____\  \ \_\ \_\  \/\_____\ 
+  \/_/\/_/   \/_/   \/____/   \/_____/   \/_____/   \/_/ /_/   \/_____/ 
+  - Kidscry#9983
+]]
+repeat wait() until game:IsLoaded();
+
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kidscry/Releases/main/Utilities/UI.lua"))();
 
-local Players = game:GetService("Players")
-local UserInputService = game:GetService("UserInputService")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-local Mouse = LocalPlayer:GetMouse()
+local Players = game:GetService("Players");
+local UserInputService = game:GetService("UserInputService");
+local RunService = game:GetService("RunService");
+local LocalPlayer = Players.LocalPlayer;
+local Mouse = LocalPlayer:GetMouse();
+
 
 -- stolen shit from wally
 if shared._unload then pcall(shared._unload) end;
 
 function shared._unload()
-	if shared._id then
-		pcall(RunService.UnbindFromRenderStep, RunService, shared._id)
-	end
 	if library.open then
 		library:Close()
 	end;
@@ -20,22 +28,22 @@ function shared._unload()
 	library.base:Destroy()
 end;
 
-local raycastParams = RaycastParams.new()
-raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+local raycastParams = RaycastParams.new();
+raycastParams.FilterType = Enum.RaycastFilterType.Blacklist;
 raycastParams.FilterDescendantsInstances = {
-	LocalPlayer.Character
+	LocalPlayer.Character;
 }
 
 local function OnRenderStepped()
-	local target = Mouse.Target
+	local target = Mouse.Target;
 	if target and target.Parent and target.Parent:IsA("Model") then
-		local humanoid = target.Parent.Humanoid
+		local humanoid = target.Parent.Humanoid;
 		if humanoid and humanoid.Health > 0 and target.Parent ~= LocalPlayer.Character then
-			local raycastResult = workspace:Raycast(Workspace.CurrentCamera.CFrame.Position, (target.Position - Workspace.CurrentCamera.CFrame.Position).Unit * 300, raycastParams)
-			if raycastResult and raycastResult.Instance == target and library.flags.Triggerbot then
-				mouse1press()
-				wait(library.flags.Delay)
-				mouse1release()
+			local raycastResult = workspace:Raycast(Workspace.CurrentCamera.CFrame.Position, (target.Position - Workspace.CurrentCamera.CFrame.Position).Unit * 300, raycastParams);
+			if raycastResult and raycastResult.Instance == target and (library.flags.Triggerbot) then
+				mouse1press();
+				wait(library.flags.Delay);
+				mouse1release();
 			end;
 		end;
 	end;
@@ -48,22 +56,37 @@ local TriggerbotToggle = Main:AddToggle({text = "Triggerbot", flag = "Triggerbot
 local Settings = library:CreateWindow("CONFIGURATIONS")
 
 Settings:AddBind({ 
-	text = 'Triggerbot Keybind',
-	key = Enum.KeyCode.Two,
+	text = 'Triggerbot Keybind';
+	key = Enum.KeyCode.Two;
 	callback = function()
-		TriggerbotToggle:SetState(not TriggerbotToggle.state)
+		TriggerbotToggle:SetState(not TriggerbotToggle.state);
 	end;
+})
+
+Settings:AddBind({ 
+	text = 'Triggerbot Hold Mode';
+	key = Enum.KeyCode.F;
+	callback = function()
+	   game.ReplicatedStorage.RemoteEvent.OnClientEvent:Connect(function()
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Kidscry",
+            Text = "Unfunctional! "
+        })
+    end)
+end;
+
 })
 Settings:AddSlider({text = 'Triggerbot Delay', flag = 'Delay', min = 0, max = 2})
 
 local Credits = library:CreateWindow("CREDITS")
-Credits:AddLabel({text='Jan - UI library'})
-Credits:AddLabel({text='Kidscry - Script'})
-Credits:AddLabel({text='Version 1.2'})
-Credits:AddLabel({text='Updated 3/12/23'})
-Credits:AddDivider()
-Credits:AddButton({text = 'Unload script', callback = function() shared._unload() end})
-Credits:AddDivider()
-Credits:AddBind({text = 'Menu toggle', key = Enum.KeyCode.Delete,callback = function() library:Close() end})
+Credits:AddLabel({text='Jan - UI library'});
+Credits:AddLabel({text='Kidscry - Script'});
+Credits:AddLabel({text='Version 1.2'});
+Credits:AddLabel({text='Updated 3/12/23'});
+Credits:AddDivider();
+Credits:AddButton({text = 'Unload script', callback = function() library.base:ClearAllChildren();
+	library.base:Destroy() end});
+Credits:AddDivider();
+Credits:AddBind({text = 'Menu toggle', key = Enum.KeyCode.Backspace, callback = function() library:Close() end})
 
-library:Init()
+library:Init();
