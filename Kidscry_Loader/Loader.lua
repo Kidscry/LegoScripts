533ce6d696b951aeddf8b1560f4ba78fbaae8f49
Kidscry <70219165+Kidscry@users.noreply.github.com>
2023-03-17 13:45:43 -0400

    Add files via upload

---
+++ Kidscry_Loader/Loader.lua
@@ -0,0 +1,18 @@
+--[[
+ __  __     __     _____     ______     ______     ______     __  __    
+/\ \/ /    /\ \   /\  __-.  /\  ___\   /\  ___\   /\  == \   /\ \_\ \   
+\ \  _"-.  \ \ \  \ \ \/\ \ \ \___  \  \ \ \____  \ \  __<   \ \____ \  
+ \ \_\ \_\  \ \_\  \ \____-  \/\_____\  \ \_____\  \ \_\ \_\  \/\_____\ 
+  \/_/\/_/   \/_/   \/____/   \/_____/   \/_____/   \/_/ /_/   \/_____/ 
+  - Kidscry#9983 * Hub Loader
+]]
+-- // Create empty table
+local placeIds = {
+    [] = "";
+};
+
+-- // Get value for PlaceId key
+local scriptUrl = placeIds[game.PlaceId];
+
+ -- // Initiate
+if scriptUrl then loadstring(game:HttpGet(scriptUrl, true))() end;
