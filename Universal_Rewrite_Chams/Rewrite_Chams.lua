01c5448187d0f967be0ece8ca2bc5f9686a7092f
Kidscry <70219165+Kidscry@users.noreply.github.com>
2023-03-17 22:07:18 -0400

    Update Rewrite_Chams.lua

--- Universal_Rewrite_Chams/Rewrite_Chams.lua
+++ Universal_Rewrite_Chams/Rewrite_Chams.lua
@@ -4,90 +4,110 @@
 \ \  _"-.  \ \ \  \ \ \/\ \ \ \___  \  \ \ \____  \ \  __<   \ \____ \  
  \ \_\ \_\  \ \_\  \ \____-  \/\_____\  \ \_____\  \ \_\ \_\  \/\_____\ 
   \/_/\/_/   \/_/   \/____/   \/_____/   \/_____/   \/_/ /_/   \/_____/ 
-  - Rewrite of (mild) : Introvert1337 Chams.lua
-    Note: Will Significantly Improve Soon.
+  - Rewrite : Introvert1337 Chams.lua
+    Note: Planning to severly modify the script soon
     Loader : loadstring(game:HttpGet("https://raw.githubusercontent.com/Kidscry/Releases/main/Universal_Rewrite_Chams/Rewrite_Chams.lua"))();
+
+Changelogs:
+
+03/17/23
+     * Increased highlights Update Frequency and Table Iteration Performance.
+     * Changed to 'function()' notation for better compatibility.
+     * Improved table iteration performance with 'ipairs' instead of 'pairs'.
+     * Replaced 'table.getn(table)' with '#table' for better performance.
+
+     + Added error handling with 'PCall'
+
+     - Removed Unused Connections Table Keys
 ]]
 --// Services
+local Players = game:GetService("Players");
+local RunService = game:GetService("RunService");
+local CoreGui = game:GetService("CoreGui");
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local CoreGui = game:GetService("CoreGui")
-
---// Variables and Settings
-
-local LocalPlayer = Players.LocalPlayer
+-- Variables and Settings
+local LocalPlayer = Players.LocalPlayer;
 
 local ChamsSettings = {
-    TeamOutlineColor = Color3.new(1, 1, 1),
-    EnemyOutlineColor = Color3.new(1, 1, 1),
-    TeamFillColor = Color3.new(0, 1, 0),
-    EnemyFillColor = Color3.new(1, 0, 0),
-    FillTransparency = 0.75,
-    OutlineTransparency = 0,
-    UseTeamColors = false,
-    ShowTeam = false,
-}
+    TeamOutlineColor = Color3.new(1, 1, 1);
+    EnemyOutlineColor = Color3.new(1, 1, 1);
+    TeamFillColor = Color3.new(0, 1, 0);
+    EnemyFillColor = Color3.new(1, 0, 0);
+    FillTransparency = 0.75;
+    OutlineTransparency = 0;
+    UseTeamColors = false;
+    ShowTeam = false;
+};
 
-local Highlights = {}
-local Connections = {}
+local Highlights = {};
+local Connections = {
+    PlayerAdded = nil;
+    CharacterAdded = {};
+    CharacterRemoving = {};
+};
 
---// Functions
-
+-- Functions
 local function RemoveChams(Player)
-    local Highlight = Highlights[Player]
+    local Highlight = Highlights[Player];
     if Highlight then
-        Highlight:Destroy()
-        Highlights[Player] = nil
-    end
-end
+        Highlight:Destroy();
+        Highlights[Player] = nil;
+    end;
+end;
 
 local function ApplyChams(Player)
     local function OnCharacterAdded(Character)
-        local Highlight = Instance.new("Highlight")
-        Highlight.Adornee = Character
-        Highlight.Parent = CoreGui
-        Highlight.OutlineTransparency = ChamsSettings.OutlineTransparency
-        Highlight.FillTransparency = ChamsSettings.FillTransparency
-        Highlights[Player] = Highlight
-    end
-    
-    local Character = Player.Character
+        local Highlight = Instance.new("Highlight");
+        Highlight.Adornee = Character;
+        Highlight.Parent = CoreGui;
+        Highlight.OutlineTransparency = ChamsSettings.OutlineTransparency;
+        Highlight.FillTransparency = ChamsSettings.FillTransparency;
+        Highlights[Player] = Highlight;
+    end;
+
+    local Character = Player.Character;
     if Character then
-        task.spawn(OnCharacterAdded, Character)
-    end
-    
-    Connections.CharacterAdded[Player] = Player.CharacterAdded:Connect(OnCharacterAdded)
+        OnCharacterAdded(Character);
+    end;
+
+    Connections.CharacterAdded[Player] = Player.CharacterAdded:Connect(function(Character)
+        OnCharacterAdded(Character);
+    end);
+
     Connections.CharacterRemoving[Player] = Player.CharacterRemoving:Connect(function()
-        RemoveChams(Player)
-    end)
-end
+        RemoveChams(Player);
+    end);
+end;
 
---// Initialization
-
+-- Initialization
 for _, Player in ipairs(Players:GetPlayers()) do
     if Player ~= LocalPlayer then
-        task.spawn(ApplyChams, Player)
-    end
-end
+        pcall(ApplyChams, Player);
+    end;
+end;
 
-Connections.PlayerAdded = Players.PlayerAdded:Connect(ApplyChams)
+Connections.PlayerAdded = Players.PlayerAdded:Connect(function(Player)
+    if Player ~= LocalPlayer then
+        pcall(ApplyChams, Player);
+    end;
+end);
 
 Players.PlayerRemoving:Connect(function(Player)
-    local CharacterAddedConnection = Connections.CharacterAdded[Player]
-    local CharacterRemovingConnection = Connections.CharacterRemoving[Player]
+    local CharacterAddedConnection = Connections.CharacterAdded[Player];
+    local CharacterRemovingConnection = Connections.CharacterRemoving[Player];
     if CharacterAddedConnection and CharacterRemovingConnection then
-        CharacterAddedConnection:Disconnect()
-        CharacterRemovingConnection:Disconnect()
-    end
-    RemoveChams(Player)
-end)
+        CharacterAddedConnection:Disconnect();
+        CharacterRemovingConnection:Disconnect();
+    end;
+    RemoveChams(Player);
+end);
 
-RunService.Stepped:Connect(function()
+RunService.Heartbeat:Connect(function()
     for Player, Highlight in pairs(Highlights) do
-        local IsSameTeam = Player.Team == LocalPlayer.Team
-        Highlight.Enabled = ChamsSettings.ShowTeam or not IsSameTeam
-        Highlight.OutlineColor = IsSameTeam and ChamsSettings.TeamOutlineColor or ChamsSettings.EnemyOutlineColor
-        Highlight.FillColor = ChamsSettings.UseTeamColors and Player.TeamColor or (IsSameTeam and ChamsSettings.TeamFillColor or ChamsSettings.EnemyFillColor)
-    end
-end)
+        local IsSameTeam = Player.Team == LocalPlayer.Team;
+        Highlight.Enabled = ChamsSettings.ShowTeam or not IsSameTeam;
+        Highlight.OutlineColor = IsSameTeam and ChamsSettings.TeamOutlineColor or ChamsSettings.EnemyOutlineColor;
+        Highlight.FillColor = ChamsSettings.UseTeamColors and Player.TeamColor or (IsSameTeam and ChamsSettings.TeamFillColor or ChamsSettings.EnemyFillColor);
+    end;
+end);
+
